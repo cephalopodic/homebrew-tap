51f5932428cf903e1122be46d8d105130a78d50d
Ian Zepp <ian.zepp@protonmail.com>
2026-04-09 12:17:58 -0500

    Initialize Homebrew tap with cephi formula

---
+++ Formula/cephi.rb
@@ -0,0 +1,31 @@
+class Cephi < Formula
+  desc "Cephalopodic CLI for discovering and invoking taps through the API"
+  homepage "https://github.com/cephalopodic/releases"
+  version "0.2.0"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/cephalopodic/releases/releases/download/cephi-v0.2.0/cephi-0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "851ad9eebe2c15f5f31ec239ebd893885aa34f01ca810e6fd0523a314b1db396"
+    end
+
+    on_intel do
+      url "https://github.com/cephalopodic/releases/releases/download/cephi-v0.2.0/cephi-0.2.0-x86_64-apple-darwin.tar.gz"
+      sha256 "49340dda889da4c9463e1c425ee887a8ea775a275c439bb02a623da746f55f8d"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/cephalopodic/releases/releases/download/cephi-v0.2.0/cephi-0.2.0-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "804c12048c88e12e8f4295b6e7264c97b5c0166e5620b5ae66ebc7ed253273bc"
+  end
+
+  def install
+    bin.install "cephi"
+  end
+
+  test do
+    assert_match "cephi", shell_output("#{bin}/cephi --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# Homebrew Tap
+
+Public Homebrew tap for Cephalopodic tools.
+
+Install with:
+
+```bash
+brew tap cephalopodic/tap
+brew install cephi
+```
+
+Or one-shot:
+
+```bash
+brew install cephalopodic/tap/cephi
+```
