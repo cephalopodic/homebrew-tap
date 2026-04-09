ad6c189fc469acf7b749692f0128f927233aece4
Ian Zepp <ian.zepp@protonmail.com>
2026-04-09 12:43:01 -0500

    Update cephi formula for v0.2.1

--- Formula/cephi.rb
+++ Formula/cephi.rb
@@ -3,26 +3,26 @@ class Cephi < Formula
 
   desc "Cephalopodic CLI for discovering and invoking taps through the API"
   homepage "https://github.com/cephalopodic/releases"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   host_os = RUBY_PLATFORM
   host_cpu = RbConfig::CONFIG.fetch("host_cpu")
 
   if host_os.include?("darwin") && host_cpu.match?(/arm|aarch64/)
-    archive = "cephi-0.2.0-aarch64-apple-darwin.tar.gz"
-    checksum = "851ad9eebe2c15f5f31ec239ebd893885aa34f01ca810e6fd0523a314b1db396"
+    archive = "cephi-0.2.1-aarch64-apple-darwin.tar.gz"
+    checksum = "1be3156a9fd781981855e1d65781507a396b2be3df3df6f383661ba4ad97e16e"
   elsif host_os.include?("darwin") && host_cpu == "x86_64"
-    archive = "cephi-0.2.0-x86_64-apple-darwin.tar.gz"
-    checksum = "49340dda889da4c9463e1c425ee887a8ea775a275c439bb02a623da746f55f8d"
+    archive = "cephi-0.2.1-x86_64-apple-darwin.tar.gz"
+    checksum = "1c967fc6fd37f048372b31bfe492a50118244512c816e0cb5aef2763a568bc69"
   elsif host_os.include?("linux")
-    archive = "cephi-0.2.0-x86_64-unknown-linux-gnu.tar.gz"
-    checksum = "804c12048c88e12e8f4295b6e7264c97b5c0166e5620b5ae66ebc7ed253273bc"
+    archive = "cephi-0.2.1-x86_64-unknown-linux-gnu.tar.gz"
+    checksum = "a05f6812a2ccb4e583d50d96b2bcb4db878246b18491d726cce9616d54b71228"
   else
     raise "Unsupported platform for cephi: #{host_os} / #{host_cpu}"
   end
 
-  url "https://github.com/cephalopodic/releases/releases/download/cephi-v0.2.0/#{archive}"
+  url "https://github.com/cephalopodic/releases/releases/download/cephi-v0.2.1/#{archive}"
   sha256 checksum
 
   def install
