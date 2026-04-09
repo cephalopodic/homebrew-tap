0a154c9f15a85657af173b8698c2939f5360f8b6
Ian Zepp <ian.zepp@protonmail.com>
2026-04-09 12:20:34 -0500

    Fix cephi formula platform URL selection

--- Formula/cephi.rb
+++ Formula/cephi.rb
@@ -4,19 +4,13 @@ class Cephi < Formula
   version "0.2.0"
   license "MIT"
 
-  on_macos do
-    on_arm do
+  if OS.mac? && Hardware::CPU.arm?
       url "https://github.com/cephalopodic/releases/releases/download/cephi-v0.2.0/cephi-0.2.0-aarch64-apple-darwin.tar.gz"
       sha256 "851ad9eebe2c15f5f31ec239ebd893885aa34f01ca810e6fd0523a314b1db396"
-    end
-
-    on_intel do
+  elsif OS.mac? && Hardware::CPU.intel?
       url "https://github.com/cephalopodic/releases/releases/download/cephi-v0.2.0/cephi-0.2.0-x86_64-apple-darwin.tar.gz"
       sha256 "49340dda889da4c9463e1c425ee887a8ea775a275c439bb02a623da746f55f8d"
-    end
-  end
-
-  on_linux do
+  elsif OS.linux?
     url "https://github.com/cephalopodic/releases/releases/download/cephi-v0.2.0/cephi-0.2.0-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "804c12048c88e12e8f4295b6e7264c97b5c0166e5620b5ae66ebc7ed253273bc"
   end
